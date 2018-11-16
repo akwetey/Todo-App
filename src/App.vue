0f08e1f98e005865432d8fc59653f82ab25e004c
akwetey <okine315@gmail.com>
2018-11-16 14:46:01 +0000

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,11 @@
     <h1 class="ui dividing centered header">Vue.js Todo App</h1>
     <div class='ui three column centered grid'>
       <div class='column'>
-        <myapp v-bind:todos="todos"></myapp>
+        <myapp v-bind:todos="todos"
+         v-on:delete-todo="deleteTodo"
+            v-on:complete-todo="completeTodo">
+                </myapp> <!-- v-on: listening for the events -->
+
         <add-todo v-on:add-todo="addTodo"></add-todo>
       </div>
     </div>
@@ -31,8 +35,37 @@ export default {
   methods: {
     addTodo(newTodo){
          this.todos.push(newTodo);
-     
-    }
+    },
+  // delete method
+    deleteTodo(todo) {
+       this.$swal({
+        title: 'Are you sure?',
+        text: 'You can\'t revert your action',
+        type: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#DD6B55',
+        confirmButtonText: 'Yes, delete it!',
+        cancelButtonText: 'No, Keep it!',
+        showCloseButton: true,
+       }).then((result) => {
+
+         if (result.value){
+         const todoIndex = this.todos.indexOf(todo);
+         this.todos.splice(todoIndex, 1)
+        this.$swal('Deleted!', 'Your Todo has been deleted', 'success')
+         }
+         else{
+           this.$swal('Canceled', 'Your Todo is still in place', 'info')
+         }
+
+       });
+    },
+   //complete method
+    completeTodo(todo) {
+      const todoIndex = this.todos.indexOf(todo);
+      this.todos[todoIndex].done = true;
+      this.$swal('Success', 'Todo Completed!', 'success');
+    },
   },
   // data supplied to main component
   data:function () {
--- src/components/Myapp.vue
+++ src/components/Myapp.vue
@@ -2,7 +2,12 @@
   <div>
     <p class="tasks">Completed Tasks: {{todos.filter(todo => {return todo.done === true}).length}}</p>
     <p class="tasks">Pending Tasks: {{todos.filter(todo => {return todo.done === false}).length}}</p>
-    <todo v-on:delete-todo="deleteTodo" v-on:complete-todo="completeTodo" v-for="todo in todos" :key="todo.id"  v-bind:todo="todo"></todo>
+    <todo
+    v-on:delete-todo="$emit('delete-todo', todo)"
+    v-on:complete-todo="$emit('complete-todo', todo)"
+    v-for="todo in todos" :key="todo.id"
+    v-bind:todo="todo">
+</todo>
   </div>
 </template>
 
@@ -20,38 +25,8 @@ components:{
   Todo,
 },
 methods: {
-  // delete method
-    deleteTodo(todo) {
-       this.$swal({
-        title: 'Are you sure?',
-        text: 'You can\'t revert your action',
-        type: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#DD6B55',
-        confirmButtonText: 'Yes, delete it!',
-        cancelButtonText: 'No, Keep it!',
-        showCloseButton: true,
 
-
-       }).then((result) => {
-         const todoIndex = this.todos.indexOf(todo);
-         this.todos.splice(todoIndex, 1)
-         if (result.value){
-           this.$swal('Deleted!', 'Your Todo has been deleted', 'success')
-         }
-         else{
-           this.$swal('Canceled', 'Your Todo is still in place', 'info')
-         }
-
-       });
-    },
-   //complete method
-    completeTodo(todo) {
-      const todoIndex = this.todos.indexOf(todo);
-      this.todos[todoIndex].done = true;
-      this.$swal('Success', 'Todo Completed!', 'success');
-    },
-  }
+  },
 };
 //end of method
 </script>
